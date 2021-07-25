88e2bd18ec6683d3cc712386b45e612be2a4e260
Glitch (glitch-hello-node) <none>
2021-07-25 11:55:29 +0000

    🎿🎥 Checkpoint ./server.js:34220115/1201 ./watch.json:34220115/6 ./package.json:34220115/3084 ./.gitignore:34220115/11

--- server.js
+++ server.js
@@ -4,17 +4,48 @@ const app = Fastify({
   logger: true
 });
 const PORT = process.env.PORT || 3000;
+const { v4: uuidv4 } = require("uuid");
 
 const storage = new Cruds("data-siswa");
 
 app.addContentTypeParser('application/json', { parseAs: 'string' }, function (req, body, done) {
   try {
-    var json = JSON.parse(body);
+    const json = JSON.parse(body);
     done(null, json);
   } catch (err) {
     err.statusCode = 400;
     done(err, undefined);
   };
+});
+
+app.get("/", (req, reply) => {
+  reply.send("Hello world");
+});
+
+app.post("/siswa", {
+  preHandler: (req, reply, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const fullName = req.body.full_name;
+    
+    if (!username || !password || !fullName) {
+      return reply.send("Missing parameters");
+    } else {
+      next();
+    }
+  }
+}, (req, reply) => {
+  const username = req.body.username.toLowerCase();
+  const password = req.body.password;
+  const fullName = req.body.full_name;
+  
+  const alls = Object.values(storage.all());
+  if (alls.find(u => u.username == username)) return reply.send("Username is already exists!");
+  const id = uuidv4().split("-")[0];
+  storage.set(id, {
+    username, password, fullName, date: Date.now()
+  });
+  reply.send(storage.get(id));
 });
 
 app.listen(PORT);
